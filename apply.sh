831494825874db4450fce469d7bfbaaf0d0e28bd
mamonaco1973 <mamonaco1973@gmail.com>
2025-01-27 16:44:04 +0000

    add v1 of docker build

--- apply.sh
+++ apply.sh
@@ -17,6 +17,24 @@ terraform apply -auto-approve
 # Return to the parent directory
 cd ..
 
+# Navigate to the 02-docker directory
+
+cd "02-docker"
+echo "NOTE: Building flask container with Docker."
+
+# Retrieve the AWS Account ID using the AWS CLI.
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+
+# Authenticate Docker to AWS ECR using the retrieved credentials.
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com
+
+# Build and push the Docker image.
+# The image tag includes the AWS Account ID and the specified repository and tag.
+
+docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com/flask-app:flask-app-rc1 . --push
+
+cd ..
+
 # Execute the validation script
 #./validate.sh
 
