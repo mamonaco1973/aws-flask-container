859df212d385d30824ab4896cb7a63edb533695f
Mike Monaco <mamonaco1973@gmail.com>
2025-01-28 11:21:38 -0500

    push working apprunner code

--- 03-apprunner/main.tf
+++ 03-apprunner/main.tf
@@ -2,3 +2,6 @@
 provider "aws" {
   region = "us-east-2" # Default region set to US East (Ohio). Modify if your deployment requires another region.
 }
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
--- 03-apprunner/roles.tf
+++ 03-apprunner/roles.tf
@@ -1,55 +1,59 @@
+# IAM Role for App Runner to run the service
 resource "aws_iam_role" "app_runner_run_role" {
- name               = "app-runner-run-role" # Name of the IAM Role
- assume_role_policy = jsonencode({    
-   Version = "2012-10-17",
-   Statement = [
-     {
-       Effect = "Allow",              
-       Principal = {
-         Service = "tasks.apprunner.amazonaws.com"
-       },
-       Action = "sts:AssumeRole"       # Action to assume the role
-     }
-   ]
- })
+  name               = "app-runner-run-role" # Name of the IAM Role
+  assume_role_policy = jsonencode({          # Assume role policy document in JSON format
+    Version = "2012-10-17",                  # Policy version
+    Statement = [
+      {
+        Effect = "Allow",                    # Allow App Runner to assume this role
+        Principal = {
+          Service = "tasks.apprunner.amazonaws.com" # Service principal for App Runner tasks
+        },
+        Action = "sts:AssumeRole"           # Action to allow assuming this role
+      }
+    ]
+  })
 }
 
+# IAM Role Policy for App Runner to interact with DynamoDB
 resource "aws_iam_role_policy" "app_runner_policy" {
- name   = "app-runner-policy"                   # Name of the policy
- role   = aws_iam_role.app_runner_run_role.id   # Attach the policy to the IAM role
- policy = jsonencode({                          # Define the policy document in JSON format
-   Version = "2012-10-17",
-   Statement = [
-     {
-       Action   = [                         # List of actions allowed on the DynamoDB table
-         "dynamodb:Query",                  # Allow querying the table
-         "dynamodb:PutItem",                # Allow adding items to the table
-         "dynamodb:Scan"                    # Allow scanning the table
-       ],
-       Effect   = "Allow",                  # Grant the specified permissions
-       Resource = "${aws_dynamodb_table.candidate-table.arn}" # Reference the table's ARN
-     }
-   ]
- })
+  name   = "app-runner-policy"                   # Name of the IAM policy
+  role   = aws_iam_role.app_runner_run_role.id   # Attach the policy to the previously created IAM role
+  policy = jsonencode({                          # Inline policy document in JSON format
+    Version = "2012-10-17",                      # Policy version
+    Statement = [
+      {
+        Action = [                               # List of DynamoDB actions allowed
+          "dynamodb:Query",                      # Allow querying items in the DynamoDB table
+          "dynamodb:PutItem",                    # Allow inserting new items into the table
+          "dynamodb:Scan"                        # Allow scanning the entire table
+        ],
+        Effect   = "Allow",                      # Grant the specified actions
+        Resource = "${aws_dynamodb_table.candidate-table.arn}" # Reference the ARN of the target DynamoDB table
+      }
+    ]
+  })
 }
 
+# IAM Role for App Runner to build the service
 resource "aws_iam_role" "app_runner_build_role" {
- name               = "app-runner-build-role" # Name of the IAM Role
- assume_role_policy = jsonencode({
-   Version = "2012-10-17",
-   Statement = [
-     {
-       Effect = "Allow",
-       Principal = {
-         Service = "build.apprunner.amazonaws.com"
-       },
-       Action = "sts:AssumeRole"       # Action to assume the role
-     }
-   ]
- })
+  name               = "app-runner-build-role" # Name of the IAM Role
+  assume_role_policy = jsonencode({            # Assume role policy document in JSON format
+    Version = "2012-10-17",                    # Policy version
+    Statement = [
+      {
+        Effect = "Allow",                      # Allow App Runner to assume this role for builds
+        Principal = {
+          Service = "build.apprunner.amazonaws.com" # Service principal for App Runner builds
+        },
+        Action = "sts:AssumeRole"             # Action to allow assuming this role
+      }
+    ]
+  })
 }
 
+# IAM Policy Attachment for ECR access
 resource "aws_iam_role_policy_attachment" "attach_ecr_policy" {
-  role       = aws_iam_role.app_runner_build_role.name                          # IAM Role to attach the policy
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+  role       = aws_iam_role.app_runner_build_role.name # Attach the policy to the App Runner build role
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess" # AWS-managed policy for ECR access
 }
