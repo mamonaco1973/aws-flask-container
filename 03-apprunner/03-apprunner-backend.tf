ea4ad79f1b37f6836f5396c13e48bbf2612aeca8
mamonaco1973 <mamonaco1973@gmail.com>
2025-02-01 23:13:58 +0000

    remove backends

--- 03-apprunner/03-apprunner-backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "824622998597-tfstate"
-    key            = "03-apprunner/terraform.tfstate.json"
-    region         = "us-east-2"
-    dynamodb_table = "824622998597-tflock"
-    encrypt        = true
-  }
-}
